fbe4b54a55ca40ade3ccd0aa5bb00640319900bc
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-23 00:17:18 +0530

    we got a chat app baby

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,29 @@
+import { useChannelMessage } from "@onehop/react";
 import type { NextPage } from "next";
 import { useState } from "react";
 import { trpc } from "../utils/trpc";
 
+type Message = {
+  id: string;
+  message: string;
+  author: string;
+};
+
 const Home: NextPage = () => {
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   const [author, setAuthor] = useState("");
+
   const createMessage = trpc.useMutation("message.create");
 
+  useChannelMessage("messages", "MESSAGE_CREATE", (data) => {
+    // @ts-ignore
+    setMessages((prevData) => [data, ...prevData]);
+  });
+
   return (
-    <div className="flex flex-col items-center mx-auto">
-      <h1>T3 HOP</h1>
+    <div className="flex flex-col w-4/5 mx-auto md:w-2/5">
+      <h1 className="text-3xl font-extrabold pb-10 pt-4">T3 HOP</h1>
 
       <form
         onSubmit={(event) => {
@@ -19,6 +33,9 @@ const Home: NextPage = () => {
             message,
             author,
           });
+
+          setMessage("");
+          setAuthor("");
         }}
         className="flex flex-col gap-2"
       >
@@ -43,6 +60,17 @@ const Home: NextPage = () => {
           className="px-4 py-2 text-lg rounded-md bg-zinc-700 cursor-pointer"
         />
       </form>
+
+      <div className="self-start pt-10">
+        {messages.map((msg: any) => {
+          return (
+            <div key={msg.id} className="py-2">
+              <b>{msg.message}</b>
+              <p>- {msg.author}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
