e5d175e2c930c16f28b6bd3590821b243b884993
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-22 23:24:37 +0530

    write mutation

--- src/server/router/message.ts
+++ src/server/router/message.ts
@@ -1,10 +1,21 @@
 import { createRouter } from "./context";
 import { z } from "zod";
+import { Hop } from "@onehop/js";
+import cuid from "cuid";
+
+// @ts-ignore
+const hop = new Hop(process.env.HOP_PROJECT_TOKEN as string);
 
 export const messageRouter = createRouter().mutation("create", {
   input: z.object({
     message: z.string(),
     author: z.string(),
   }),
-  async resolve() {},
+  async resolve({ input: { message, author } }) {
+    await hop.channels.publishMessage("messages", "MESSAGE_CREATE", {
+      message,
+      author,
+      id: cuid(),
+    });
+  },
 });
