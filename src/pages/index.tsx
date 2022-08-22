967fafa7a5002802534ac0886fc23ae25cd4151b
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-22 23:52:41 +0530

    hook up form with mutation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,49 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import { trpc } from "../utils/trpc";
+
 const Home: NextPage = () => {
+  const [message, setMessage] = useState("");
+  const [author, setAuthor] = useState("");
+  const createMessage = trpc.useMutation("message.create");
+
   return (
-    <>
+    <div className="flex flex-col items-center mx-auto">
       <h1>T3 HOP</h1>
-    </>
+
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          createMessage.mutate({
+            message,
+            author,
+          });
+        }}
+        className="flex flex-col gap-2"
+      >
+        <input
+          type="text"
+          placeholder="message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          className="rounded-md px-4 py-2 text-lg bg-zinc-800 focus:outline-none focus:ring-1 focus:ring-zinc-600"
+        />
+        <input
+          type="text"
+          placeholder="author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+          className="rounded-md px-4 py-2 text-lg bg-zinc-800 focus:outline-none focus:ring-1 focus:ring-zinc-600"
+        />
+
+        <input
+          type="submit"
+          value="Send"
+          className="px-4 py-2 text-lg rounded-md bg-zinc-700 cursor-pointer"
+        />
+      </form>
+    </div>
   );
 };
 
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,9 +1,10 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
 import superjson from "superjson";
+import { messageRouter } from "./message";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-
+  .merge("message.", messageRouter);
 // export type definition of API
 export type AppRouter = typeof appRouter;
